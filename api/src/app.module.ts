e1b22477a2bf8ff4bf284b8a855992682174bd52
megathyne <megathyne@gmail.com>
2020-02-16 00:01:45 -0500

    added vars for db in api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -13,12 +13,11 @@ import { IssueModule } from './issue/issue.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // host: 'localhost',
       host: process.env.DB_HOST,
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'internal_comic_signal',
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
       entities: [__dirname + '/**/*.entity.{js,ts}'],
       synchronize: true,
     }),
