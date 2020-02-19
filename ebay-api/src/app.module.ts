19be28db6cac8c5494be48955a5e1100f75d09d7
ceej989 <cjohn989@gmail.com>
2020-02-19 16:53:53 -0500

    fixed docker configs

--- ebay-api/src/app.module.ts
+++ ebay-api/src/app.module.ts
@@ -7,11 +7,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.EBAY_DB_HOST,
-      port: parseInt(process.env.EBAY_DB_PORT),
-      username: process.env.EBAY_DB_USERNAME,
-      password: process.env.EBAY_DB_PASSWORD,
-      database: process.env.EBAY_DB_DATABASE_NAME,
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
       entities: [__dirname + '/**/*.entity.{js,ts}'],
       synchronize: true,
     }),
