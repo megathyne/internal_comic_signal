5b10e791eb1ab27b6d83fcef8a059fcbe6b549a2
ceej989 <ceej989@live.com>
2020-03-08 16:47:33 -0400

    fixed migrations

--- ebay-api/src/app.module.ts
+++ ebay-api/src/app.module.ts
@@ -3,19 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FindingModule } from './finding/finding.module';
 import { FileUploadModule } from './file-upload/file-upload.module';
 import { EbayItemModule } from './ebay-item/ebay-item.module';
+import * as ormconfig from './ormconfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity.{js,ts}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(ormconfig),
     FindingModule,
     FileUploadModule,
     EbayItemModule,
