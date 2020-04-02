beec4b0ca0d1e15baf18c1cbdf8a2a084305250d
megathyne <megathyne@gmail.com>
2020-04-01 23:00:01 -0400

    fix login

--- client/src/screen/Login/index.js
+++ client/src/screen/Login/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component, useState, useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -9,60 +9,67 @@ import Grid from '@material-ui/core/Grid';
 import { loginSaga } from '../../actions';
 import styles from './styles';
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.handleBtnOnClick = this.handleBtnOnClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    marginTop: '30vh',
+  },
+}));
 
-  handleChange = input => event => {
-    this.setState({ [input]: event.target.value });
+export default function Login(props) {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const listener = event => {
+      if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+        dispatch(loginSaga(data));
+      }
+    };
+    document.addEventListener('keydown', listener);
+    return () => {
+      document.removeEventListener('keydown', listener);
+    };
+  }, []);
+
+  const handleChange = input => event => {
+    const d = data;
+    d[input] = event.target.value;
+    setData(d);
   };
 
-  handleBtnOnClick() {
-    this.props.loginSaga({
-      username: this.state.username,
-      password: this.state.password,
-    });
-  }
+  const handleBtnOnClick = () => {
+    dispatch(loginSaga(data));
+  };
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              id="standard-username-input"
-              label="username"
-              variant="outlined"
-              onChange={this.handleChange('username')}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="standard-password-input"
-              label="Password"
-              type="password"
-              variant="outlined"
-              autoComplete="current-password"
-              onChange={this.handleChange('password')}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" color="primary" onClick={this.handleBtnOnClick}>
-              Login
-            </Button>
-          </Grid>
-        </Grid>
+  return (
+    <div className={classes.root}>
+      <div>
+        <div style={{ marginBottom: '4%' }}>
+          <TextField
+            id="standard-username-input"
+            label="username"
+            variant="outlined"
+            onChange={handleChange('username')}
+          />
+        </div>
+        <div style={{ marginBottom: '4%' }}>
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            variant="outlined"
+            autoComplete="current-password"
+            onChange={handleChange('password')}
+          />
+        </div>
+        <Button variant="contained" type="submit" fullWidth color="primary" onClick={handleBtnOnClick}>
+          Login
+        </Button>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-const mapDispatchToProps = dispatch => ({
-  loginSaga: user => dispatch(loginSaga(user)),
-});
-
-export default connect(null, mapDispatchToProps)(withStyles(styles)(Login));
