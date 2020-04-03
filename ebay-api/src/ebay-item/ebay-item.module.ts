b186706a09c8d254f9063bcc1b3d8329cc8d47f0
ceej989 <ceej989@live.com>
2020-04-03 00:30:24 -0400

    getting single ebay item

--- ebay-api/src/ebay-item/ebay-item.module.ts
+++ ebay-api/src/ebay-item/ebay-item.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { EbayItemService } from './ebay-item.service';
 import { EbayItemController } from './ebay-item.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EbayItemRepository } from './ebay-item.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EbayItemRepository])],
+  imports: [TypeOrmModule.forFeature([EbayItemRepository]), HttpModule],
   providers: [EbayItemService],
   controllers: [EbayItemController],
   exports: [EbayItemService, TypeOrmModule],
